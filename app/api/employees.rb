b1a5034e5aab15ae3767a6033501ab5e03043743
Higor Moreno Ribeiro <higormorenoribeiro@gmail.com>
2016-04-09 13:19:21 -0300

    tratamento de work_day no employee

--- app/api/employees.rb
+++ app/api/employees.rb
@@ -11,16 +11,27 @@ class Employees < Grape::API
     end
 
     desc "create a employee"
-    params do
-      requires :name, type: String
-      requires :company_id, type: Integer
-    end
     post do
       employee = Employee.new({
-        name: params[:name],
-        company_id: params[:company_id]
+        name: params.name,
+        company_id: params.company_id
         })
+
+      params.work_days.each do |param|
+        smt = param.start_morning_time.split(':')
+        emt = param.end_morning_time.split(':')
+        sat = param.start_afternoon_time.split(':')
+        eat = param.end_morning_time.split(':')
+        employee.WorkDays << WorkDay.new({
+          day: param.day,
+          start_morning_time: Time.new(2000,01,01,smt[0],smt[1],00, "+00:00"),
+          end_morning_time: Time.new(2000,01,01,emt[0],emt[1],00, "+00:00"),
+          start_afternoon_time: Time.new(2000,01,01,sat[0],sat[1],00, "+00:00"),
+          end_afternoon_time: Time.new(2000,01,01,eat[0],eat[1],00, "+00:00")
+          })
+      end
       employee.save!
+      employee
     end
 
     desc "deletes a employee"
